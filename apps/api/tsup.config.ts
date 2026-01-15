c89f9f752a2beeca702769d85a5d8013e30f3f6a
Vinicius <viniciusaugustooliveira2007@gmail.com>
2026-01-15 12:15:38 -0300

    refactor: API Dockerfile and update build config

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/server.ts"],
+  outDir: "dist",
+  target: "node18",
+  platform: "node",
+  format: ["cjs"],
+  bundle: true,
   splitting: false,
   sourcemap: true,
   clean: true,
