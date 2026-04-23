4ad487055038dd6babd47b56c7953fe72c9a4e54
Vinicius <viniciusaugustooliveira2007@gmail.com>
2026-04-23 12:30:58 -0300

    chore: remove unused products action file

--- apps/web/src/shared/actions/products.ts
+++
@@ -1,7 +0,0 @@
-
-const findAllProducts = async () => {
-  const response = await API.get<FindAllProductsResponse>("/products");
-  return response.data;
-};
-
-export const productService = { findAllProducts };
