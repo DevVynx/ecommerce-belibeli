17feab9e9042898d1b17140542bb895adc401f3d
Vinicius <viniciusaugustooliveira2007@gmail.com>
2026-06-23 14:55:58 -0300

    fix(web/cart): add response type to removeItemFromCart

--- apps/web/src/shared/actions/cart/removeItem.ts
+++ apps/web/src/shared/actions/cart/removeItem.ts
@@ -1,14 +1,17 @@
 "use server";
 
-import type { RemoveItemFromCartRequest } from "@repo/types/contracts";
+import type { RemoveItemFromCartRequest, RemoveItemFromCartResponse } from "@repo/types/contracts";
 
 import { fetchClient } from "@/shared/utils/api/fetchClient";
 
 export async function removeItemFromCart(params: RemoveItemFromCartRequest) {
-  const { error } = await fetchClient<void>(`/cart/items/${params.cartItemId}`, {
-    isPrivate: true,
-    method: "DELETE",
-  });
+  const { error } = await fetchClient<RemoveItemFromCartResponse>(
+    `/cart/items/${params.cartItemId}`,
+    {
+      isPrivate: true,
+      method: "DELETE",
+    }
+  );
 
   if (error) return { data: null, error };
 
