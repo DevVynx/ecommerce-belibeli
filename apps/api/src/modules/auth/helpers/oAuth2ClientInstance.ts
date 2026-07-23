c4af05c972afeacd0f9c4537f60b6dca1e3f80f5
Vinicius <viniciusaugustooliveira2007@gmail.com>
2026-07-23 12:03:58 -0300

    fix(api/auth): use object constructor for OAuth2Client
    - Migrate OAuth2Client from positional args to object constructor
      to align with Google API deprecation

--- apps/api/src/modules/auth/helpers/oAuth2ClientInstance.ts
+++ apps/api/src/modules/auth/helpers/oAuth2ClientInstance.ts
@@ -2,8 +2,8 @@ import { OAuth2Client } from "google-auth-library";
 
 import { ENV } from "@/shared/utils/env";
 
-export const client = new OAuth2Client(
-  ENV.GOOGLE_CLIENT_ID,
-  ENV.GOOGLE_CLIENT_SECRET,
-  "postmessage"
-);
+export const client = new OAuth2Client({
+  client_id: ENV.GOOGLE_CLIENT_ID,
+  client_secret: ENV.GOOGLE_CLIENT_SECRET,
+  redirectUri: "postmessage",
+});
